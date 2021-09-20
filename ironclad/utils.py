3b0c68f2274c3720f9932d493f2f792426a2eb14
mlysaght <michael.a.lysaght@gmail.com>
2021-09-20 15:06:28 +0100

    Add in cryptography

--- ironclad/utils.py
+++ ironclad/utils.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 """Helper utilities and decorators."""
 from flask import flash
+from cryptography.hazmat.primitives.asymmetric import dsa
+from cryptography.hazmat.backends import default_backend
+
 
 
 def flash_errors(form, category="warning"):
@@ -8,3 +11,8 @@ def flash_errors(form, category="warning"):
     for field, errors in form.errors.items():
         for error in errors:
             flash(f"{getattr(form, field).label.text} - {error}", category)
+
+    private_key = dsa.generate_private_key(
+        key_size=512,
+        backend=default_backend()
+    )   
